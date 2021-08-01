edc119beeb95f6001c5cc7c17710148685894213
Samuel-de-Oliveira <samwolfg13@gmail.com>
2021-08-01 19:37:33 -0300

    not done

--- Main.py
+++ Main.py
@@ -1,20 +1,22 @@
 from time import sleep
 from winConf import *
 
-print('CanUseTimer Terminal\'s version BETA')
+print('CanUseTimer Terminal\'s version: 0.1 BETA')
 
 while True:
-    sleep(0.4)
-    line()
-    print("1: \033[0;30;47mStart\n"
+    sleep(0.3)
+    line(style='doble_line')
+    print("1: Start\n"
           "2: Change modality\n"
-          "3: Exit"
-          )
-    line()
+          "3: Exit")
+    line(style='doble_line')
+
     console = int(input('>>: '))
+    print()
 
     if console == 1: window('Start')
     if console == 2: window('Change modality')
     if console == 3: break
+    print()
 
 print('Bye, bye!')
--- winConf.py
+++ winConf.py
@@ -1,8 +1,9 @@
 def line(size=40, style='basic'):
-    if style == 'basic':
-        print('-' * size)
+    if style == 'basic': print('-' * size)
+    if style == 'doble_line': print('=' * size)
+    if style == 'hashtag': print('#' * size)
 
 def window(msg='[Null]', style='basic'):
-    line(len(msg)+6, style)
-    print(f'|  {msg}  |')
-    line(len(msg)+6, style)
+    line(len(msg) + 10, style)
+    print(f'|    {msg}    |')
+    line(len(msg) + 10, style)
