f7b5224816946a7b3f9bb641e23938d7bb22fcc5
Samuel de Oliveira <81493939+Samuel-de-Oliveira@users.noreply.github.com>
2021-08-20 11:38:45 -0300

    Version: 0.1.3 BETA

--- unistall.sh
+++ unistall.sh
@@ -5,7 +5,7 @@ sudo pip3 uninstall keyboard # unistalling keyboard library.
 if [ -f /bin/canusetimer-terminal ]; then
     sudo rm /bin/canusetimer-terminal # removing executer.
 fi
-if [ -d /opt/CanUseTimer-terminal/ ]; then
-    sudo rm -rf /opt/CanUseTimer-terminal/ # removing program directory.
+if [ -d /opt/CanUseTimer-Terminal/ ]; then
+    sudo rm -rf /opt/CanUseTimer-Terminal/ # removing program directory.
 fi
 echo "Everything is removed!"
