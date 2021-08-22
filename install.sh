c982574aaf13872bb6d8042d9cea3b144dcaed39
Samuel de Oliveira <81493939+Samuel-de-Oliveira@users.noreply.github.com>
2021-08-22 12:38:18 -0300

    Updating installer

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 echo "The installing starts..."
 # install keyboard library and pip if don't have
 if [ -f '/etc/debian_version' ]; then # Debian based distribuition
-    sudo apt install python3-pip
+    sudo apt install python3-pip -y
     sudo pip3 install keyboard
 elif [ -f '/etc/arch-release' ]; then # Arch based distribuition
-    sudo pacman -Syu python-pip
-    sudo pip3 install keyboard
+    sudo pacman -Syy python-pip
+    sudo pip install keyboard
 else # if your Linux distro is not supported exit
     echo "Your linux distribuition is not supported, sorry. :("
     exit 1
