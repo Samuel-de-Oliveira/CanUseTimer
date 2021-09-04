c3b6d67f16c296ec5ca57912c4761a982b12925d
Samuel-de-oliveira <samwolfg13@gmail.com>
2021-09-04 18:58:06 +0000

    Linux installer update

--- install.sh
+++ install.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-echo "The installing starts!"
-# install keyboard library and pip if don't have
-echo "Installing libraries"
-if [ -f '/etc/debian_version' ]; then # Debian based distribuition
-    sudo apt install python3-pip -y
-    sudo pip3 install keyboard
-elif [ -f '/etc/arch-release' ]; then # Arch based distribuition
-    sudo pacman -Syy python-pip
-    sudo pip install keyboard
-else # if your Linux distro is not supported exit
-    echo "Your linux distribuition is not supported, sorry. :("
-    exit 1
+echo -e "This program will install the following packages:\n\
+python3_pip, keyboard_python_lib"
+echo -e "\nand will create a folder in /opt/"
+
+echo -e "\nAre you sure you want to install? [Y/n]: ";read num
+
+if [ $num == 'y' ] || [ $num == 'Y' ]; then
+	echo "The installing starts!"
+	# install keyboard library and pip if don't have
+	echo "Installing libraries"
+	if [ -f '/etc/debian_version' ]; then # Debian based distribuition
+	    sudo apt install python3-pip -y
+	    sudo pip3 install keyboard
+	elif [ -f '/etc/arch-release' ]; then # Arch based distribuition
+	    sudo pacman -Syy python-pip
+	    sudo pip install keyboard
+	else # if your Linux distro is not supported exit
+	   echo "Your linux distribuition is not supported, sorry. :("
+	   exit 1
+	fi
+
+	echo "Creating executer..."
+	sudo cp lib/canusetimer-terminal /bin/
+
+	echo "Creting directories and files..."
+	if [ ! -d /opt/CanUseTimer-Terminal ]; then
+	    sudo mkdir /opt/CanUseTimer-Terminal
+	fi
+	sudo cp *.py /opt/CanUseTimer-Terminal/
+	sudo cp -rf lib/ /opt/CanUseTimer-Terminal/
+	
+	echo "Everything is done!"
+else
+	echo "abort!"
 fi
-
-echo "Creating executer..."
-sudo cp lib/canusetimer-terminal /bin/
-
-echo "Creting directories and files..."
-if [ ! -d /opt/CanUseTimer-Terminal ]; then
-    sudo mkdir /opt/CanUseTimer-Terminal
-fi
-sudo cp *.py /opt/CanUseTimer-Terminal/
-sudo cp -rf lib/ /opt/CanUseTimer-Terminal/
-
-echo "Everything is done!"
--- unistall.sh
+++ unistall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Removing the program..."
+echo "Are you sure you want to unistall? [Y/n]"; read num
+
+if [ $num == 'y' ] || [$num == 'Y']; then
+echo -e "Removing the program..."
 sudo pip3 uninstall keyboard # unistalling keyboard library.
 if [ -f /bin/canusetimer-terminal ]; then
     sudo rm /bin/canusetimer-terminal # removing executer.
@@ -9,3 +12,6 @@ if [ -d /opt/CanUseTimer-Terminal/ ]; then
     sudo rm -rf /opt/CanUseTimer-Terminal/ # removing program directory.
 fi
 echo "Everything is removed!"
+else
+echo "Abort!"
+fi
