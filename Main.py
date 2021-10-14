f77a4a9ef055ece27bd23f216bd2faf9372ecfe8
Samuel-de-Oliveira <samwolfg13@gmail.com>
2021-10-14 12:22:34 -0300

    need fix a bug

--- Main.py
+++ Main.py
@@ -4,15 +4,15 @@ import json
 from sys import argv
 
 param = argv[1:]
-
 consoleClear()
-with open('lib/setting.json', 'r') as f: setting = json.loads(f.read())
+settings()
+
 if len(param) == 0:
     print('CanUseTimer\'s version: 0.2 BETA.\n'
           'This software is open to free use and study code,\n'
           'for more info: https://github.com/samuel-de-oliveira/CanUseTimer.\n')
     while True:
-        with open('lib/setting.json', 'r') as f: setting = json.loads(f.read())
+        settings()
         line(style='double_line')
         print('Digit a one of these numbers:\033[36m')
         print("1: Start\n"
@@ -22,30 +22,30 @@ if len(param) == 0:
               "5: Exit\033[m")
         line(style='double_line')
         console = Console(size=5)
+
         consoleClear()
         if console == 1: 
             window('Starting timer...')
-            startTimer(setting['modality'])
-        if console == 2 :
+            startTimer(settings.load['modality'])
+        if console == 2:
             window('Settings')
-            settingManager()
+            settings.manager('')
         if console == 3:
-            timesSave.clear() 
+            timesSave.clear()
             window('The cube\'s times is cleared!', 'double_line')
         if console == 4:
             window('Credits to:', 'double_line')
             print('The creator: Samuel de Oliveira')
         if console == 5: break
 
-elif param[0] == '--change-modality' or '-c':
-    print(type(param[0]))
+elif param[0] in ('--change-modality', '-c'):
     try:
         print(param[1])
         if param[1] in ('3x3', '2x2', '4x4', '5x5', 'pyra', 'skewb'):
-            setting['modality'] = argv[2]
-            with open('lib/setting.json', 'w+') as f: f.write(json.dump(setting))
+            settings.load['modality'] = argv[2]
+            with open('lib/setting.json', 'w+') as f: f.write(json.dump(settings.load))
         else: print('Digit a valid modality!')
 
     except: print('digit: canusetimer -c [modality name]')
 
-elif param[0] == '--start' or '-s': startTimer(setting['modality'])
+elif param[0] in ('--start', '-s'): startTimer(settings.load['modality'])
--- lib/__init__.py
+++ lib/__init__.py
@@ -8,37 +8,56 @@ import json
 timesSave = []
 class settings():
     def __init__(self):
-        with open('lib/setting.json', 'r') as f: self.s = json.loads(f.read())
-        if len(self.s) >= 2:
-            with open('lib/setting.json', 'r') as f:
-                json.dump({'modality': '3x3', 'ask+2': True}, indent=True)
-                self.s = json.loads(f.read())
+        with open('lib/setting.json', 'r') as f: self.load = json.loads(f.read())
 
-    def manager(self): 
+        #if len(self.load) >= 2:
+         #   with open('lib/setting.json', 'w+') as f:
+        #        json.dumps({'modality': '3x3', 'ask+2': True}, indent=True)
+       #         self.load = json.loads(f.read())
+
+    def manager(self):
         while True:
             line(style='double_line')
             print('\033[36m1: Change Modality\n'
-                  f'2: ask +2 >> {self.s["ask+2"]}\n'
-                '3: Close\033[m')
+            f'2: ask +2 >> {self.load["ask+2"]}\n'
+            '3: Close\033[m')
             line(style='double_line')
             numget = Console(size=3)
 
             consoleClear()
             if numget == 1:
                 window('Change modality.')
-                defModality(self.s['modality'])
+                self.Modality(self.load['modality'])
             if numget == 2: self.askP2()
             if numget == 3:
                 window('Settings\'s closed', 'double_line')
                 break
 
-    def askP2():
-        if self.s['ask+2']: self.s['ask+2'] = False
-        else: self.s['ask+2'] = True
+    def askP2(self):
+        if self.load['ask+2']: self.load['ask+2'] = False
+        else: self.load['ask+2'] = True
 
-        with open('lib/setting.json', 'w+') as f: f.write(json.dumps(self.s, indent=True))
-        window(f'ask +2 now is: {self.s["ask+2"]}', 'double_line')
+        with open('lib/setting.json', 'w+') as f: f.write(json.dumps(self.load, indent=True))
+        window(f'ask +2 now is: {self.load["ask+2"]}', 'double_line')
 
+    def Modality(self):
+        modals = ('3x3', '2x2', '4x4', '5x5', 'pyra', 'skewb')
+        print('All modalities:', end='')
+        for m in modals: print(f' {m}', end=' ')
+
+        print() 
+        x = input('Digit the modality name: ')
+        consoleClear()
+        if x in modals:
+            timesSave.clear()
+            window('Modality changed!', 'double_line')
+            setting['modality'] = x
+            with open('lib/setting.json', 'w') as f: f.write(json.dumps(self.load, indent=True))
+            return x
+        else:
+            window('this modality doesn\'t exist.', 'double_line')
+            return modality
+ 
 def consoleClear():
     if os.name in ('nt', 'dos'): os.system('cls')
     else: os.system('clear')
@@ -74,28 +93,7 @@ def showAverage():
         timesUse /= len(timesSave) - 2
 
         return float(f'{timesUse:.2f}')
-
-
-
-
-def defModality(modality):
-    modals = ('3x3', '2x2', '4x4', '5x5', 'pyra', 'skewb')
-    print('All modalities:', end='')
-    for m in modals: print(f' {m}', end=' ')
-
-    print()
-    x = input('Digit the modality name: ')
-    consoleClear()
-    if x in modals:
-        timesSave.clear()
-        window('Modality changed!', 'double_line')
-        setting['modality'] = x
-        with open('lib/setting.json', 'w') as f: f.write(json.dumps(setting, indent=True))
-        return x
-    else:
-        window('this modality doesn\'t exist.', 'double_line')
-        return modality
-
+    
 def startTimer(modality):
     modalities = {'3x3': Salete('3x3'),
                   '2x2': Salete('2x2'),
