9625b5582f2f53cf7a95b2308850aba5beaebbce
Samuel-de-Oliveira <samwolfg13@gmail.com>
2022-03-11 10:20:00 -0300

    Update installer

--- install.sh
+++ install.sh
@@ -13,12 +13,23 @@ if [ $num == 'y' ] || [ $num == 'Y' ]; then
 	# The program only move the python files to /opt/ and create a executer in /bin/
 	# Is more simple than you think
 	echo -e "\nThe installing starts! This not will take long.\n"
-	
+
+	echo "Checking if Git is installed"
+	if [ ! -f /usr/bin/git ]; then
+		if [ -f /etc/debian_version ]; then
+			sudo apt install git -y
+		elif [ -f /etc/arch-release ]; then
+			sudo pacman -Sy git | yes
+		else
+			echo -e "\033[31mYour distribuition is not supported :(\033[m"
+			exit 1
+		fi
+	fi
+
 	if [ ! -d /opt/CanUseTimer/ ]; then
 		echo "Creating main directorie in /opt/..."
         	sudo mkdir /opt/CanUseTimer/
 	else
-	# Remove cache
 		echo "removing cache"
 		if [ -d /opt/CanUseTimer/__pycache__ ]; then
 			sudo rm -rf /opt/CanUseTimer/__pycache__
@@ -28,8 +39,11 @@ if [ $num == 'y' ] || [ $num == 'Y' ]; then
 		fi
 	fi
 	
+	echo "Cloning library repository"
+	git clone https://github.com/boppreh/keyboard
+
 	echo "Creating Keyboard Python lib..."
-	sudo cp -rf keyboard/ /opt/CanUseTimer/
+	sudo mv -f keyboard/ /opt/CanUseTimer/
 
 	echo "Creating executer..."
 	sudo cp lib/canusetimer /usr/bin/
