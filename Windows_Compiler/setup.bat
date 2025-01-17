5a17873645557fa6c1f901aeb73a8b4c43d062af
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-01-16 23:10:43 -0300

    Update Windows Setup

--- Windows_Compiler/setup.bat
+++ Windows_Compiler/setup.bat
@@ -1,2 +1,3 @@
-pip install -r ..\requirements.txt
-pyinstaller -F ..\Main.py -i ..\Images\icon.ico
+python -m venv ..
+..\scripts\pip install -r ..\requirements.txt
+..\scripts\pyinstaller -F ..\Main.py -i ..\Images\icon.ico
