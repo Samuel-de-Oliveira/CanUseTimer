4658d107c89d11db6165fdee24004aa380c4150f
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-01-24 11:46:25 -0300

    Add all modalities on data base

--- lib/data.py
+++ lib/data.py
@@ -23,4 +23,60 @@ times_cursor.execute("""
   );
 """)
 
+# 4x4Cube
+times_cursor.execute("""
+  CREATE TABLE IF NOT EXISTS Cube4x4 (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time REAL NOT NULL
+  );
+""")
+
+# 5x5Cube
+times_cursor.execute("""
+  CREATE TABLE IF NOT EXISTS Cube5x5 (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time REAL NOT NULL
+  );
+""")
+
+# 6x6Cube
+times_cursor.execute("""
+  CREATE TABLE IF NOT EXISTS Cube6x6 (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time REAL NOT NULL
+  );
+""")
+
+# 7x7Cube
+times_cursor.execute("""
+  CREATE TABLE IF NOT EXISTS Cube7x7 (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time REAL NOT NULL
+  );
+""")
+
+# Pyranminx
+times_cursor.execute("""
+  CREATE TABLE IF NOT EXISTS Pyra (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time REAL NOT NULL
+  );
+""")
+
+# Skewb
+times_cursor.execute("""
+  CREATE TABLE IF NOT EXISTS Skewb (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time REAL NOT NULL
+  );
+""")
+
+# Square one
+times_cursor.execute("""
+  CREATE TABLE IF NOT EXISTS SQ1 (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time REAL NOT NULL
+  );
+""")
+
 if __name__ == "__main__": print("Run the Main.py file to start the program...")
